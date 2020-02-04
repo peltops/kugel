155682833026c7869b47ede38a7270e0dfebb878
Developer Peltops <developer@peltops.com>
2020-02-04 16:24:16 +0100

    [developer][tutorial] initial config

---
+++ src/main/java/com/peltops/kugel/KugelApplication.java
@@ -0,0 +1,13 @@
+package com.peltops.kugel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KugelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KugelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peltops/kugel/KugelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peltops.kugel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KugelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
